3466281e4589b3e231262cd29c3f5a366b9d3769
musabgelisgen <mgelisgen97@gmail.com>
2020-04-23 15:01:19 +0300

    enum types are added for browser and os

---
+++ src/main/java/bilshort/link/models/BrowserType.java
@@ -0,0 +1,24 @@
+package bilshort.link.models;
+
+public enum BrowserType{
+    CHROME("chrome"),
+    FIREFOX("firefox"),
+    SAFARI("safari"),
+    INTERNET_EXPLORER("internet explorer"),
+    OTHER_BROWSER("other_browser");
+
+    private final String value;
+
+    BrowserType(String value){
+        this.value = value;
+    }
+
+    public String value(){
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return value();
+    }
+}
